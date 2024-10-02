d46e7dba850bc8805ccb1013ea44716514ee9bed
Marina Laguna <marinalaguna2004@gmail.com>
2024-10-02 12:59:58 +0200

    Update usuario.js

--- back/models/usuario.js
+++ back/models/usuario.js
@@ -10,7 +10,13 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      this.belongsToMany(models.Rol, {
+        through: models.UserRols,
+        foreignKey: "id_user",
+        otherKey: "id_rol",
+        as: "roles",
+        onDelete: "CASCADE",
+      });
     }
   }
   Usuario.init({
