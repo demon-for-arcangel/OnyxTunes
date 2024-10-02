a5b68edd740d675e16fe1d7df64b674f4334628d
Marina Laguna <marinalaguna2004@gmail.com>
2024-10-02 12:59:01 +0200

    Creación conexiones
    creacion del archivo connection.js y el archivo userConnection.js para la conexion a la base de datos con la tabla Usuario

---
+++ back/database/user/UserConnection.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+const { Sequelize, Op } = require("sequelize");
+const models = require("../../models");
+const Conexion = require("../connection.js");
+
+const conexion = new Conexion();
