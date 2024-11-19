44bdc9e8184ba295e88ef9c1e0f8d86c0fedc59b
Marina <marinalaguna2004@gmail.com>
2024-11-19 10:00:22 +0100

    Actualizar usuario funcional

--- front/src/app/components/management/update/update-user/update-user.component.ts
+++ front/src/app/components/management/update/update-user/update-user.component.ts
@@ -4,6 +4,8 @@ import { UserService } from '../../../../services/user.service';
 import { FormsModule } from '@angular/forms';
 import { DynamicDialogConfig } from 'primeng/dynamicdialog';
 import { CommonModule } from '@angular/common';
+import { Rol } from '../../../../interfaces/usuario';
+import { RolService } from '../../../../services/rol.service';
 
 @Component({
   selector: 'app-update-user',
@@ -23,13 +25,16 @@ export class UpdateUserComponent {
   };
 
   rolUsuario: string = ""
+  roles: Rol[] = []
+  selectedRole: string = '';
 
-  constructor(private userService: UserService, private router: Router, private config: DynamicDialogConfig) {}
+  constructor(private userService: UserService, private rolService: RolService, private router: Router, private config: DynamicDialogConfig) {}
 
   ngOnInit(): void {
     this.usuarioId = this.config.data.usuarioId; 
     if (this.usuarioId) {
       this.getUserById(this.usuarioId); 
+      this.loadRoles();
     } else {
       console.error('No se proporcionó el usuarioId');
     }
@@ -38,21 +43,31 @@ export class UpdateUserComponent {
   getUserById(id: string): void {
     this.userService.getUserById(id).subscribe(data => {
       this.usuario = data; 
-
-      if (this.usuario.roles && Array.isArray(this.usuario.roles)) {
-        this.rolUsuario = this.usuario.roles
-          .map((role: any) => role.nombre)
-          .join(', ');
+      if (this.usuario.roles.length > 0) {
+        this.selectedRole = this.usuario.roles[0].nombre;
       }
+
     }, error => {
       console.error('Error al obtener el usuario:', error);
     });
   }
 
+  loadRoles(): void {
+    this.rolService.getRoles().subscribe(data => {
+      this.roles = data;
+    }, error => {
+      console.error('Error al cargar los roles:', error);
+    })
+  }
+
   onSubmit(): void {
+    this.usuario.roles = { nombre: this.selectedRole.trim() };
     this.userService.updateUser(this.usuario.id, this.usuario).subscribe(response => {
       alert('Usuario actualizado exitosamente'); //cambiar alert
-      this.router.navigate(['/userManagement']);
+      setTimeout(() => {
+        window.location.reload();
+      }, 2000);
+
     }, error => {
       console.error('Error al actualizar el usuario', error);
     });
