110c1ad527073cb47d4b1f11ea879989bcd3f455
Marina <marinalaguna2004@gmail.com>
2024-11-30 17:27:09 +0100

    Update rolController.js

--- back/controllers/rolController.js
+++ back/controllers/rolController.js
@@ -15,28 +15,14 @@ const getRoles = async (req, res) => {
 
 const getRolById = async (req, res) => {
     try {
-        // Obtener el ID de los parámetros de la solicitud
         const { id } = req.params;
 
-        // Validar que el ID sea un número
-        if (isNaN(id)) {
-            return res.status(400).json({ msg: "El ID debe ser un número" });
-        }
-
-        // Llamar a la función del modelo para obtener el rol por ID
         const rol = await conx.getRolById(id);
-
-        // Devolver el rol encontrado
         res.status(200).json(rol);
     } catch (error) {
-        console.error('Error al obtener el rol por ID', error);
-
-        // Manejar el error de rol no encontrado
         if (error.message === 'User no encontrado') {
             return res.status(404).json({ msg: "Rol no encontrado" });
         }
-
-        // Manejar otros errores
         res.status(500).json({ msg: "Error al obtener el rol" });
     }
 };
