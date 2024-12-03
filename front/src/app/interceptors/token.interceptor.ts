320ce09641f17b37a2f6416d9f4bc703759748a9
Marina <marinalaguna2004@gmail.com>
2024-12-03 08:07:15 +0100

    Tokenn interceptor

--- front/src/app/interceptors/token.interceptor.ts
+++ front/src/app/interceptors/token.interceptor.ts
@@ -1,5 +1,15 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+  const token = localStorage.getItem('user'); // O de donde estés almacenando el token
+
+  if (token) {
+    // Clona la solicitud y agrega el token a las cabeceras
+    const clonedRequest = req.clone({
+      headers: req.headers.set('x-token', token)
+    });
+    return next(clonedRequest); // Envía la solicitud clonada
+  }
+
+  return next(req); // Envía la solicitud original si no hay token
 };
