b7041596fb22d0ad02c19e55df6a6c21e9c1987f
Marina <marinalaguna2004@gmail.com>
2024-12-05 10:33:49 +0100

    creado componente y servicio para la busqueda

--- front/src/app/components/search/search.component.ts
+++ front/src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SearchService } from '../../services/search.service';
 
 @Component({
   selector: 'app-search',
@@ -8,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
+  query: string = '';  // Almacena lo que el usuario escribe
+  results: any[] = []; // Almacena los resultados de la búsqueda
 
+  constructor(private searchService: SearchService) {}
+
+  // Método para realizar la búsqueda
+  search() {
+    if (this.query.trim()) {
+      this.searchService.search(this.query).subscribe(response => {
+        this.results = response;
+      }, error => {
+        console.error('Error en la búsqueda:', error);
+      });
+    }
+  }
 }
