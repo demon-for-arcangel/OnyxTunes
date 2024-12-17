f4c9fa3a15c61a1bc0aff7e410e8202c370c6d8e
Marina Laguna Valdepeñas <marinalaguna2004@gmail.com>
2024-12-17 16:41:57 +0100

    send Mail in th e interface

--- front/src/app/components/resetPassword/request-reset/request-reset.component.ts
+++ front/src/app/components/resetPassword/request-reset/request-reset.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MailService } from '../../../services/mail.service';
+import { CommonModule, Location } from '@angular/common';
 
 @Component({
   selector: 'app-request-reset',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule, ReactiveFormsModule],
   templateUrl: './request-reset.component.html',
   styleUrl: './request-reset.component.css'
 })
 export class RequestResetComponent {
+  resetForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder, private mailServices: MailService, private router: Router) {
+    this.resetForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  ngOnInit(): void {}
+
+  sendResetEmail() {
+    if (this.resetForm.valid) {
+      const email = this.resetForm.get('email')?.value;
+      this.mailServices.sendMail({email: email}).subscribe(
+        (response: any) => { 
+          this.router.navigate(['/']); 
+        },
+        (error: any) => { 
+          console.error('Error al enviar el correo de restablecimiento', error);
+        }
+      );
+    }
+  }
 }
