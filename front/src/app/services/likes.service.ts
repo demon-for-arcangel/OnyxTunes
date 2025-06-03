a8627f81f25a540c96f91e1146782d1fd6803b7b
Marina Laguna Valdepeñas <marinalaguna2004@gmail.com>
2025-06-03 16:51:37 +0200

    proteccion de rutas para like routes

--- front/src/app/services/likes.service.ts
+++ front/src/app/services/likes.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
 import { LikeResponse } from '../interfaces/like';
 
 @Injectable({
@@ -15,10 +15,34 @@ export class LikesService {
   constructor(private http: HttpClient) { }
 
   getLikesByUserId(userId: number): Observable<LikeResponse> {
-    return this.http.get<LikeResponse>(this.url + this.likesUrl + `/user/${userId}`)
+    const userls = localStorage.getItem("user");
+    const userData = userls ? JSON.parse(userls) : null;
+    const token = userData?.token;
+
+    if (!token) {
+      return throwError(() => new Error("No hay token en la petición."));
+    }
+
+    const headers = new HttpHeaders({
+      "x-token": token, 
+    });
+
+    return this.http.get<LikeResponse>(`${this.url}${this.likesUrl}/user/${userId}`, { headers });
   }
 
   deleteLike(likeId: number): Observable<any> {
-    return this.http.delete<any>(`${this.url}${this.likesUrl}/${likeId}`);
+    const userls = localStorage.getItem("user");
+    const userData = userls ? JSON.parse(userls) : null;
+    const token = userData?.token;
+
+    if (!token) {
+      return throwError(() => new Error("No hay token en la petición."));
+    }
+
+    const headers = new HttpHeaders({
+      "x-token": token, 
+    });
+
+    return this.http.delete<any>(`${this.url}${this.likesUrl}/${likeId}`, { headers });
   }
 }
