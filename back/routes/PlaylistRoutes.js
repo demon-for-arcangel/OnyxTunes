3459299293ceee0f40ac1260872c28ba21554bd6
Marina Laguna Valdepeñas <marinalaguna2004@gmail.com>
2025-06-03 16:46:04 +0200

    proteccion de rutas para playlist routes

--- back/routes/PlaylistRoutes.js
+++ back/routes/PlaylistRoutes.js
@@ -1,20 +1,21 @@
 const { Router } = require("express");
 const playlistController = require("../controllers/playlistController");
+const { checkToken } = require('../middlewares/abilities');
+
 const router = Router();
 
 router.get("/", playlistController.index); 
 router.get("/:id", playlistController.getPlaylistById); 
-router.post("/new", playlistController.createPlaylist); 
+router.post("/new", checkToken, playlistController.createPlaylist); 
 router.put("/:id", playlistController.updatePlaylist); 
 router.delete("/", playlistController.deletePlaylists);
-router.get('/user/:userId', playlistController.getUserPlaylists);
+router.get('/user/:userId', checkToken, playlistController.getUserPlaylists);
 router.post('/:userId', playlistController.createPlaylistByUser);
 
-router.post('/song/like', playlistController.addToFavorites);
-router.delete('/song/delete', playlistController.deleteSongPlaylist);
-
-router.post("/genre/createPlaylist", playlistController.createPlaylistsByGenres);
-router.post("/add/songs", playlistController.addSongsToPlaylist);
+router.post('/song/like', checkToken, playlistController.addToFavorites);
+router.delete('/song/delete', checkToken, playlistController.deleteSongPlaylist);
+router.post("/genre/createPlaylist", checkToken, playlistController.createPlaylistsByGenres);
+router.post("/add/songs", checkToken, playlistController.addSongsToPlaylist);
 
 
 module.exports = router;
--- front/src/app/services/playlist.service.ts
+++ front/src/app/services/playlist.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,8 +14,20 @@ export class PlaylistService {
   private playlistUrl = environment.playlistUrl
 
 
-  getUserPlaylists(userId: number): Observable<any> {
-    return this.http.get<any>(`${this.url}` + `${this.playlistUrl}` + `/user/${userId}`); 
+ getUserPlaylists(userId: number): Observable<any> {
+    const userRawData = localStorage.getItem("user");
+    const userData = userRawData ? JSON.parse(userRawData) : null;
+    const token = userData?.token;
+
+    if (!token) {
+      return throwError(() => new Error("No hay token en la petición."));
+    }
+
+    const headers = new HttpHeaders({
+      "x-token": token, 
+    });
+
+    return this.http.get<any>(`${this.url}${this.playlistUrl}/user/${userId}`, { headers });
   }
 
   getPlaylistById(id: number): Observable<any> {
@@ -23,31 +35,87 @@ export class PlaylistService {
   }
 
   addToFavorites(songId: number, userId: number): Observable<any> {
-    return this.http.post<any>(this.url + this.playlistUrl + '/song/like', { songId, userId });
+    const userRawData = localStorage.getItem("user");
+    const userData = userRawData ? JSON.parse(userRawData) : null;
+    const token = userData?.token;
+
+    if (!token) {
+      return throwError(() => new Error("No hay token en la petición."));
+    }
+
+    const headers = new HttpHeaders({
+      "x-token": token, 
+    });
+
+    return this.http.post<any>(`${this.url}${this.playlistUrl}/song/like`, { songId, userId }, { headers });
   }
 
   createPlaylist(playlistData: { nombre: string; descripcion: string; usuario_id: number; publico: boolean; canciones?: number[] }): Observable<any> {
-    return this.http.post<any>(`${this.url}${this.playlistUrl}/new`, playlistData);
+    const userls = localStorage.getItem("user");
+    const userData = userls ? JSON.parse(userls) : null;
+    const token = userData?.token;
+
+    if (!token) {
+      return throwError(() => new Error("No hay token en la petición."));
+    }
+
+    const headers = new HttpHeaders({
+      "x-token": token,
+    });
+
+    return this.http.post<any>(`${this.url}${this.playlistUrl}/new`, playlistData, { headers });
   }
 
   deleteSongPlaylist(songId: number, playlistId: number): Observable<any> {
-    return this.http.delete<any>(this.url + this.playlistUrl + '/song/delete', { 
-      body: { 
-        songId, 
-        playlistId 
-      } 
+    const userls = localStorage.getItem("user");
+    const userData = userls ? JSON.parse(userls) : null;
+    const token = userData?.token;
+
+    if (!token) {
+      console.error("❌ No hay token disponible.");
+      return throwError(() => new Error("No hay token en la petición."));
+    }
+
+    const headers = new HttpHeaders({
+      "x-token": token, 
+    });
+
+    return this.http.delete<any>(`${this.url}${this.playlistUrl}/song/delete`, {
+      body: { songId, playlistId },
+      headers,
     });
   }
 
   createPlaylistsByGenres(): Observable<any> {
-    return this.http.post(`${this.url}${this.playlistUrl}/genre/createPlaylist`, {});
+    const userRawData = localStorage.getItem("user");
+    const userData = userRawData ? JSON.parse(userRawData) : null;
+    const token = userData?.token;
+
+    if (!token) {
+      return throwError(() => new Error("No hay token en la petición."));
+    }
+
+    const headers = new HttpHeaders({
+      "x-token": token, 
+    });
+
+    return this.http.post(`${this.url}${this.playlistUrl}/genre/createPlaylist`, {}, { headers });
   }
 
   addSongsToPlaylist(userId: number, sourcePlaylistId: number, targetPlaylistId: number): Observable<any> {
-    return this.http.post<any>(`${this.url}${this.playlistUrl}/add/songs`, {
-      userId,
-      sourcePlaylistId,
-      targetPlaylistId
+    const userls = localStorage.getItem("user");
+    const userData = userls ? JSON.parse(userls) : null;
+    const token = userData?.token;
+
+    if (!token) {
+      return throwError(() => new Error("No hay token en la petición."));
+    }
+
+    const headers = new HttpHeaders({
+      "x-token": token,
     });
+
+    return this.http.post<any>(`${this.url}${this.playlistUrl}/add/songs`, 
+      { userId, sourcePlaylistId, targetPlaylistId }, { headers });
   }
 }
