a9a5335e0bd13ce3607e1390aa0e7be34f183d6a
Marina Laguna Valdepeñas <marinalaguna2004@gmail.com>
2025-06-03 16:33:15 +0200

    proteccion de rutas para playlist favoritas

--- front/src/app/services/playlistfavorite.service.ts
+++ front/src/app/services/playlistfavorite.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,15 +14,54 @@ export class PlaylistfavoriteService {
   private playlistFavoriteUrl = environment.playlistFavoriteUrl
  
   getFavoritePlaylists(usuarioId: number): Observable<any> {
-    return this.http.get(`${this.url}${this.playlistFavoriteUrl}/${usuarioId}`);
+    const userRawData = localStorage.getItem("user");
+    const userData = userRawData ? JSON.parse(userRawData) : null;
+    const token = userData?.token;
+
+    if (!token) {
+      return throwError(() => new Error("No hay token en la petición."));
+    }
+
+    const headers = new HttpHeaders({
+      "x-token": token,
+    });
+
+    return this.http.get(`${this.url}${this.playlistFavoriteUrl}/${usuarioId}`, { headers });
   }
 
   addFavoritePlaylist(usuarioId: number, playlistId: number): Observable<any> {
-    return this.http.post(`${this.url}${this.playlistFavoriteUrl}/add`, { usuarioId, playlistId });
+    const userRawData = localStorage.getItem("user");
+    const userData = userRawData ? JSON.parse(userRawData) : null;
+    const token = userData?.token;
+
+    if (!token) {
+      return throwError(() => new Error("No hay token en la petición."));
+    }
+
+    const headers = new HttpHeaders({
+      "x-token": token, 
+    });
+
+    return this.http.post(`${this.url}${this.playlistFavoriteUrl}/add`, { usuarioId, playlistId }, { headers });
   }
 
   removeFavoritePlaylist(usuarioId: number, playlistId: number): Observable<any> {
-    return this.http.delete(`${this.url}${this.playlistFavoriteUrl}/remove`, { body: { usuarioId, playlistId } });
+    const userRawData = localStorage.getItem("user");
+    const userData = userRawData ? JSON.parse(userRawData) : null;
+    const token = userData?.token;
+
+    if (!token) {
+      return throwError(() => new Error("No hay token en la petición."));
+    }
+
+    const headers = new HttpHeaders({
+      "x-token": token,
+    });
+
+    return this.http.delete(`${this.url}${this.playlistFavoriteUrl}/remove`, {
+      body: { usuarioId, playlistId },
+      headers,
+    });
   }
 
 }
